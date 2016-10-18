3c2bcd8bb30d47c31b33fe48feb811270f15be15
sujoyDohatec <sujoy@dohatec.com.bd>
2016-10-18 17:29:30 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+declare let $: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  signup(){
+    $('#myModal').modal('show');
+  }
 }
