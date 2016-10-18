d0825a8c36bcf1669d3a12a564ebb56464248b8b
sujoyDohatec <sujoy@dohatec.com.bd>
2016-10-18 18:49:28 +0600

    email verficaition added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,42 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup} from '@angular/forms';
 
 declare let $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [FormBuilder]
 })
 export class AppComponent {
   title = 'app works!';
 
-  signup(){
-    $('#myModal').modal('show');
+  userForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.userForm = this.formBuilder.group({
+      email: ['']
+    });
   }
+  signup(){
+    console.log(this.userForm.value);
+    // if (this.userForm.value.test(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/)) {
+    //     console.log("correct");
+    // }
+
+     let EMAIL_REGEXP = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+
+     if(EMAIL_REGEXP.test(this.userForm.value.email)){
+       //console.log("correct");
+       $('#myModal').modal('show');
+     }
+     else console.log("incorrect");
+     //
+  }
+
+
+
 }
